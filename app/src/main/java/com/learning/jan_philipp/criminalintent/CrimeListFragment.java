24b3c7bdd119ba25b767bc9ee2e8f5bbf1288484
janPhil <heinrich.jan@googlemail.com>
2014-07-17 17:22:22 +0200

    UI inserted

---
+++ app/src/main/java/com/learning/jan_philipp/criminalintent/CrimeListFragment.java
@@ -0,0 +1,22 @@
+package com.learning.jan_philipp.criminalintent;
+
+import android.app.ListFragment;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+/**
+ * Created by janPhil on 17.07.14.
+ */
+public class CrimeListFragment extends ListFragment {
+
+    private ArrayList<Crime> mCrimes;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        getActivity().setTitle(R.string.crimes_title);
+        mCrimes = CrimeLab.get(getActivity()).getmCrimes();
+
+    }
+}
